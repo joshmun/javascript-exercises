f08138c19ffc5c4c40dbada0378deecba36b1df9
joshmun <joshuamun@gmail.com>
2017-12-01 17:12:36 -0800

    add first practice problems on part 2

---
+++ intro-js/practice.js
@@ -0,0 +1,50 @@
+function madLib(string1, string2, string3){
+  console.log("We shall " + string1.toUpperCase() + " the " + string2.toUpperCase() + " " + string3.toUpperCase() + ".")
+}
+
+function isSubstring(searchString, subString){
+  var strings = searchString.split(" ");
+  return (strings.indexOf(subString) > -1);
+}
+
+function fizzBuzz(array){
+  var newArray = [];
+  for (var i in array){
+    if (i % 3 === 0 && i % 5 === 0){
+      continue;
+    }
+    else if (i % 3 == 0 || i%5==0){
+      newArray.push(i);
+    }
+  }
+  return newArray;
+}
+
+// fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
+function titlelize(array, callback){
+  let titled = array.map(name => "Mx. " + name + " Jingleheimer Shmidt");
+  callback(titled); 
+}
+
+function printCallback(array){
+  array.forEach(function(name){
+    console.log(name);
+  })
+}
+
+titlelize(["Mary", "Brian", "Leo"], printCallback);
+
+function Elephant(name, height, tricks){
+  this.name = name;
+  this.height = height;
+  this.arrayOftricks = tricks;
+}
+
+Elephant.prototype.trumpet = function(){
+  console.log(this.name + " the elephant goes 'phrRRRRRRRRRRR!!!!!!!'");
+}
+
+Elephant.prototype.grow = function(){
+  this.height += 12;
+}
