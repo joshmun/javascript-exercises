d087587505e2d90f40dbfe563086ec48d917ee16
joshmun <joshuamun@gmail.com>
2017-12-04 12:31:34 -0800

    refactor, big O n, from n squared

--- intro-js/w5d3_intro-js.js
+++ intro-js/w5d3_intro-js.js
@@ -1,11 +1,14 @@
 function my_uniq(array) {
   let mySet = new Set();
+  let newArray = new Array();
   array.forEach(function(item){
     if (!mySet.has(item)){
       mySet.add(item);
+      newArray.push(item);
     }
   })
-  return mySet;
+  delete mySet;
+  return newArray;
 }
 
 function my_twoSum() {
