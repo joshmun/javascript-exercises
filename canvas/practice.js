38808106e9c15717554a78c015d9cea92d65d6ca
joshmun <joshuamun@gmail.com>
2017-12-07 16:45:13 -0800

    draw rectangles and circles on canvas

--- canvas/practice.js
+++ canvas/practice.js
@@ -1,4 +1,17 @@
-
 document.addEventListener("DOMContentLoaded", function(){
+  var canvas = document.getElementById('myCanvas');
+  canvas.width = 500;
+  canvas.height = 500;
+  var ctx = canvas.getContext('2d');
 
+  ctx.fillStyle = '#ff6961';
+  ctx.fillRect(0, 0, 500, 500);
+
+  ctx.beginPath();
+  ctx.arc(250, 250, 200, 0, 2*Math.PI);
+  ctx.strokeStyle = '#aec6cf';
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.fillStyle='#aec6cf';
+  ctx.fill();
 });
